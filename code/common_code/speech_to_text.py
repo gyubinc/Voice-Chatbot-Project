0aeec63958ed97d719561a043f9a66493105942a
cheongsu <103344737+cheongsu@users.noreply.github.com>
2023-06-30 23:50:25 +0900

    Update speech_to_text.py
    input 내에 text 형태로 speech가 저장됨

--- code/common_code/speech_to_text.py
+++ code/common_code/speech_to_text.py
@@ -1,21 +1,48 @@
+# app.py
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
 from time import sleep
 
-import streamlit as st
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
 
 # 페이지 기본 설정
 st.set_page_config(
     page_icon="🐶",
-    page_title="침착맨연KU소 스트림릿",
+    page_title="임청수의 스트림릿",
     layout="wide",
 )
 
+# # # 로딩바 구현하기
+# # with st.spinner(text="페이지 로딩중..."):
+# #     sleep(2)
+
+# # 페이지 헤더, 서브헤더 제목 설정
+# st.header("임청수 페이지에 오신걸 환영합니다👋")
+# st.subheader("스트림릿 기능 맛보기")
+
+# # 페이지 컬럼 분할(예: 부트스트랩 컬럼, 그리드)
+# cols = st.columns((1, 1, 2))
+# cols[0].metric("10/11", "15 °C", "2")
+# cols[0].metric("10/12", "17 °C", "2 °F")
+# cols[0].metric("10/13", "15 °C", "2")
+# cols[1].metric("10/14", "17 °C", "2 °F")
+# cols[1].metric("10/15", "14 °C", "-3 °F")
+# cols[1].metric("10/16", "13 °C", "-1 °F")
+
+# # 라인 그래프 데이터 생성(with. Pandas)
+# chart_data = pd.DataFrame(
+#     np.random.randn(20, 3),
+#     columns=['a', 'b', 'c'])
+
+# # 컬럼 나머지 부분에 라인차트 생성
+# cols[2].line_chart(chart_data)
+
+import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
 stt_button = Button(label="Speak", width=100)
 
@@ -49,3 +76,5 @@ result = streamlit_bokeh_events(
 if result:
     if "GET_TEXT" in result:
         st.write(result.get("GET_TEXT"))
+        input = result.get("GET_TEXT")
+print(input)
